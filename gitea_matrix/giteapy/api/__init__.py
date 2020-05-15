8db2ffa2360fab39100427230c9b1a9bb1e3d2b7
saces <saces@c-base.org>
2020-05-16 00:40:56 +0200

    add generated gitea api into tree

---
+++ gitea_matrix/giteapy/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .admin_api import AdminApi
+from .issue_api import IssueApi
+from .miscellaneous_api import MiscellaneousApi
+from .notification_api import NotificationApi
+from .organization_api import OrganizationApi
+from .repository_api import RepositoryApi
+from .user_api import UserApi
